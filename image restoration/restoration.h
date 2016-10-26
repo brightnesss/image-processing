d7661f1fed9fe5534a32314073259bf9eaf4ee9d
brightnesss <brightness_j@hotmail.com>
2016-10-26 15:23:28 +0800

    a new image restoration is pushed

---
+++ image restoration/restoration.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <opencv2\opencv.hpp>
+
+void restoration(cv::Mat&, cv::Mat&, unsigned int);
---
+++ image restoration/stdafx.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define Max 100
+#define Arithmetic_Filter 1000
+#define Geometric_Filter 2000
+#define Harmonic_Filter 3000
+#define Median_Filter 4000
+
+#include <opencv2\opencv.hpp>
+#include <iostream>
+#include <string>
+#include "imgnoise.h"
+#include "random.h"
+#include <time.h>
+#include "restoration.h"
+#include "myArithmetic.h"
+#include "myGeometric.h"
+#include "myHarmonic.h"
+#include "myMedian.h"
+#include <vector>
